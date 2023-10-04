d377beb1b87d8270945c4c0917f77bd00f9d32d4
Mohammed Abdullah Almuhaitheef <141905441+Muhaitheef@users.noreply.github.com>
2023-10-04 19:00:19 +0300

    First try
    I made some works on it

--- 281Project/src/Euclidean_algorithm.java
+++ 281Project/src/Euclidean_algorithm.java
@@ -2,7 +2,22 @@ import java.util.Scanner;
 //Welcome to the Euclidean Algorithm
 public class Euclidean_algorithm {
 	public static int Euclidean(int a, int b) {
-		return 0;
+		int r = 1;
+		if (a > b) {
+			int temp = a;
+			a = b;
+			b = temp;
+		
+		}
+		
+
+		System.out.println("Step 1: Using the Euclidean Algorithm to find the GCD of " + a + " and " + b);
+		//Here is the problem 
+		while(r!=0){
+		System.out.println(b + " = " + (b / a)  + " * " + a + " + " + r);
+		}
+		System.out.println("The GCD is " + a);
+		return a;
 	}
 
 	public static void main(String[] args) {
