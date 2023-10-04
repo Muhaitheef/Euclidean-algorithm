faea4937dea191790ecd417344acf9dd4b567414
Mohammed Abdullah Almuhaitheef <141905441+Muhaitheef@users.noreply.github.com>
2023-10-04 18:39:21 +0300

    In

---
+++ 281Project/src/Euclidean_algorithm.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Euclidean_algorithm {
+	public static int Euclidean(int a, int b) {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the first Number");
+		int a = input.nextInt();
+		System.out.println("Enter the second Number");
+		int b = input.nextInt();
+		Euclidean(a, b);
+	}
+}
