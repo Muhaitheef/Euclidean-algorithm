1cb8f0faf977d1e255f4cbbf393e838efcc0ba9d
Mohammed Abdullah Almuhaitheef <141905441+Muhaitheef@users.noreply.github.com>
2023-10-18 22:42:11 +0300

    Finishing the GCD
    I did the gcd method for now , now we need to do the linear combanation

--- 281Project/src/Euclidean_algorithm.java
+++ 281Project/src/Euclidean_algorithm.java
@@ -2,20 +2,26 @@ import java.util.Scanner;
 //Welcome to the Euclidean Algorithm
 public class Euclidean_algorithm {
 	public static int Euclidean(int a, int b) {
-		int r = 1;
+		int r =b%a;
 		if (a > b) {
 			int temp = a;
 			a = b;
 			b = temp;
-		
 		}
-		
-
 		System.out.println("Step 1: Using the Euclidean Algorithm to find the GCD of " + a + " and " + b);
 		//Here is the problem 
 		while(r!=0){
+			 r =b%a;
 		System.out.println(b + " = " + (b / a)  + " * " + a + " + " + r);
+		b = a;
+		a = r;
+		r = b%a;
+		if(r==0){
+			System.out.println(b + " = " + (b / a)  + " * " + a + " + " + r);
 		}
+		}
+
+		
 		System.out.println("The GCD is " + a);
 		return a;
 	}
