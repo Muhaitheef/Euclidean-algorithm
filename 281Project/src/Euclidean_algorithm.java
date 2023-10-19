9e8619f573b44a3077a6d22f03de42a090291ef9
Mohammed Abdullah Almuhaitheef <141905441+Muhaitheef@users.noreply.github.com>
2023-10-19 17:36:31 +0300

    More changis

--- 281Project/src/Euclidean_algorithm.java
+++ 281Project/src/Euclidean_algorithm.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 //Welcome to the Euclidean Algorithm
 public class Euclidean_algorithm {
-	public static int Euclidean(int a, int b) {
+	public static void Euclidean(int a, int b) {
 		int r =b%a;
 		if (a > b) {
 			int temp = a;
 			a = b;
 			b = temp;
 		}
-		System.out.println("Step 1: Using the Euclidean Algorithm to find the GCD of " + a + " and " + b);
-		//Here is the problem 
+		System.out.println("-----------------------------------");
+		System.out.println(" Step 1: Using the Euclidean Algorithm to find the GCD of " + a + " and " + b);
+		
 		while(r!=0){
 			 r =b%a;
 		System.out.println(b + " = " + (b / a)  + " * " + a + " + " + r);
@@ -20,17 +21,21 @@ public class Euclidean_algorithm {
 			System.out.println(b + " = " + (b / a)  + " * " + a + " + " + r);
 		}
 		}
+		System.out.println("The GCD is " + a);
+		System.out.println("-----------------------------------\n-----------------------------------");
+		System.out.println(" Step 2: Working Backwards");
+		int s=1;
+		int t =0;
+		System.out.println(a + " = " + a);
 
 		
-		System.out.println("The GCD is " + a);
-		return a;
 	}
 	//hi 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the first Number");
+		System.out.println("Enter the first Number :");
 		int a = input.nextInt();
-		System.out.println("Enter the second Number");
+		System.out.println("Enter the second Number :");
 		int b = input.nextInt();
 		Euclidean(a, b);
 	}
