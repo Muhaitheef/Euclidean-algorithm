f57b13a99bb276076c4396eddebdcc43b4113e33
mohmmed alz <140457479+mohmmadalz@users.noreply.github.com>
2023-11-22 18:01:01 +0300

    Update Node.java

--- 281Project/src/Node.java
+++ 281Project/src/Node.java
@@ -1,63 +1,54 @@
 public class Node {
-    private int bigger, multi, smaller, reminder;
-    private Node next;
-
-    // Constructor for the Node class
-    public Node(int b, int m, int s, int r) {
-        bigger = b;    // b is the bigger number 
-        multi = m;    // m is the multiplier for the bigger number 
-        smaller = s;   	// s is the smaller number
-		reminder = r;    // r is the reminder
-        next = null;   
-    }
-
-    // Getter for bigger
-    public int getBigger() {
-        return bigger;
-    }
-
-    // Setter for bigger
-    public void setBigger(int bigger) {
-        this.bigger = bigger;
-    }
-
-    // Getter for multi
-    public int getMulti() {
-        return multi;
-    }
-
-    // Setter for multi
-    public void setMulti(int multi) {
-        this.multi = multi;
-    }
-
-    // Getter for smaller
-    public int getSmaller() {
-        return smaller;
-    }
-
-    // Setter for smaller
-    public void setSmaller(int smaller) {
-        this.smaller = smaller;
-    }
-
-    // Getter for reminder
-    public int getReminder() {
-        return reminder;
-    }
-
-    // Setter for reminder
-    public void setReminder(int reminder) {
-        this.reminder = reminder;
-    }
-
-    // Getter for the next Node
-    public Node getNext() {
-        return next;
-    }
-
-    // Setter for the next Node
-    public void setNext(Node next) {
-        this.next = next;
-    }
-}
+		
+	    private int bigger, multi, smaller, reminder;
+	    private Node next;
+	
+	    public Node(int b, int s, int m, int r) {
+	        bigger = b;    // b is the bigger number 
+	        multi = m;    // m is the multiplier for the bigger number 
+	        smaller = s;   	// s is the smaller number
+			reminder = r;    // r is the reminder
+	        next = null;   
+	    }
+	
+	    public int getBigger() {
+	        return bigger;
+	    }
+	
+	    public void setBigger(int bigger) {
+	        this.bigger = bigger;
+	    }
+	
+	    public int getMulti() {
+	        return multi;
+	    }
+	
+	    public void setMulti(int multi) {
+	        this.multi = multi;
+	    }
+	
+	    public int getSmaller() {
+	        return smaller;
+	    }
+	
+	    public void setSmaller(int smaller) {
+	        this.smaller = smaller;
+	    }
+	
+	    public int getReminder() {
+	        return reminder;
+	    }
+	
+	    public void setReminder(int reminder) {
+	        this.reminder = reminder;
+	    }
+	
+	    public Node getNext() {
+	        return next;
+	    }
+	
+	    public void setNext(Node next) {
+	        this.next = next;
+	    }
+	    
+	}
